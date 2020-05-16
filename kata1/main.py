df285cd1ee7977f824fd3b0df41bbbd02a8924b2
Chmorod <christianmr92@gmail.com>
2020-05-16 10:15:14 +0200

    Pruebas kata1

---
+++ kata1/main.py
@@ -0,0 +1,14 @@
+print('hello, world!')
+'''Hola esto es un adelanto del Hackathon'''
+# Variables
+nombre = 'Manuel S. Lemos'
+edad = 25
+esta_Trabajando = True
+# Datos de entrada y salida en pantalla
+dato = input('Introduce un dato: ')
+print (dato)
+#Condicionales --> con tabulación sabemos lo que pertenece a un if
+if( edad >= 18 ):
+    print('Eres '+ nombre +' y eres mayor de edad')
+else:
+    print('Eres '+ nombre +' y eres un crio')
